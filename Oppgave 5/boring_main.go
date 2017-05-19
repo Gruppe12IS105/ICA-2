37fdc528f63f59834005e17c5ca1950d430902cc
Konflux <marign16@student.uia.no>
2017-05-19 10:27:50 +0200

    Update boring_main.go

--- Oppgave 5/boring_main.go
+++ Oppgave 5/boring_main.go
@@ -3,22 +3,11 @@ package main
 
 import (
 	"os"
-
 	"./boring"
-	//"fmt"
 )
 
 func main() {
 	msg := os.Args[1]
 	boring.Boring01(msg)
 
-	/*
-	  c := make(chan string) //oppretter channel c i denne filen
-
-	  go boring.Boring10("Index nr: ", c) //starter func Boring10 i den andre koden
-	  for i := 0; i < 1; i++{             //denne for-løkken kjører parallellt med Boring10
-	    fmt.Printf("Test er: %q\n", <-c)
-	  }
-	  fmt.Println("Neiass, dette gidder jeg ikke")
-	*/
 }
